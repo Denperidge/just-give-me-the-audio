da73341c86d49799eae5f30c1f247def197183aa
Denperidge <denperidge@gmail.com>
2020-07-19 10:09:33 +0200

    Fixed recusive updating error

--- setup/Download-Scripts.ps1
+++ setup/Download-Scripts.ps1
@@ -1,12 +1,13 @@
 $scriptDir = "app"
+$updatingSelf = "updating-self"
 
 # If not in app directory, create and move towards it
 if (!(Test-Path "Variables.ps1")) {
     New-Item -Name $scriptDir -ItemType "directory"
     Move-Item -Path @("Download-Scripts.ps1", "install.bat") -Destination $scriptDir
     Set-Location $scriptDir
-} else {
-    # If already in the app directory, also update self
+} elseif (!$args[1] -match $updatingSelf) {
+    # If already in the app directory and not already updating self, also update self
     Invoke-WebRequest "https://raw.githubusercontent.com/Denperidge/just-give-me-the-audio/main/setup/Download-Scripts.ps1" -OutFile "Download-Scripts.ps1"
     Invoke-WebRequest "https://raw.githubusercontent.com/Denperidge/just-give-me-the-audio/main/setup/install.bat" -OutFile "install.bat"
     # Run new updater and exit self woops
@@ -18,7 +19,7 @@ Invoke-WebRequest "https://raw.githubusercontent.com/Denperidge/just-give-me-the
 Invoke-WebRequest "https://raw.githubusercontent.com/Denperidge/just-give-me-the-audio/main/app/Download-Binaries.ps1" -OutFile "Download-Binaries.ps1"
 Invoke-WebRequest "https://raw.githubusercontent.com/Denperidge/just-give-me-the-audio/main/app/Download-Video.ps1" -Outfile "Download-Video.ps1"
 
-& .\Download-Binaries.ps1
+& .\Download-Binaries.ps1 $updatingSelf
 . .\Variables.ps1
 
 Set-Location ..
