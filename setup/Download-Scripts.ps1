89992a9bdc3337e691842795d85e028d87cae11f
Denperidge <denperidge@gmail.com>
2020-07-19 09:51:27 +0200

    Added missing -OutFile parameters

--- setup/Download-Scripts.ps1
+++ setup/Download-Scripts.ps1
@@ -4,9 +4,9 @@ New-Item -Name $scriptDir -ItemType "directory"
 Move-Item -Path ./* -Destination "setup" 
 Set-Location $scriptDir
 
-Invoke-WebRequest "https://raw.githubusercontent.com/Denperidge/just-give-me-the-audio/main/app/Variables.ps1"
-Invoke-WebRequest "https://raw.githubusercontent.com/Denperidge/just-give-me-the-audio/main/app/Download-Binaries.ps1"
-Invoke-WebRequest "https://raw.githubusercontent.com/Denperidge/just-give-me-the-audio/main/app/Download-Video.ps1"
+Invoke-WebRequest "https://raw.githubusercontent.com/Denperidge/just-give-me-the-audio/main/app/Variables.ps1" -OutFile "Variables.ps1"
+Invoke-WebRequest "https://raw.githubusercontent.com/Denperidge/just-give-me-the-audio/main/app/Download-Binaries.ps1" -OutFile "Download-Binaries.ps1"
+Invoke-WebRequest "https://raw.githubusercontent.com/Denperidge/just-give-me-the-audio/main/app/Download-Video.ps1" -Outfile "Download-Video.ps1"
 
 & .\Download-Binaries.ps1
 . .\Variables.ps1
--- setup/install.bat
+++ setup/install.bat
@@ -1,2 +1,2 @@
-PowerShell Invoke-WebRequest https://raw.githubusercontent.com/Denperidge/just-give-me-the-audio/main/setup/Download-Scripts.ps1
+PowerShell Invoke-WebRequest "https://raw.githubusercontent.com/Denperidge/just-give-me-the-audio/main/setup/Download-Scripts.ps1" -OutFile "Download-Scripts.ps1"
 powershell -ExecutionPolicy ByPass "Download-Scripts.ps1"
