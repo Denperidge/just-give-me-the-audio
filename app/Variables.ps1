bdcfeb2028b9b067010672db685dc31e0d3f7655
Denperidge <denperidge@gmail.com>
2023-04-18 20:17:49 +0200

    Reverted back to previous BinariesDir

--- app/Variables.ps1
+++ app/Variables.ps1
@@ -1,9 +1,7 @@
-$BinariesDir = "$PSScriptRoot/bin"
+$BinariesDir = "bin"
 $OutputDir = "../output/"
 $ffmpegUnzipdir = "$BinariesDir/ffmpeg/"
 $ffmpegZipname = "ffmpeg.zip"
 $youtubeDLname = "$BinariesDir/yt-dlp.exe"
 $AudioFormats = @("aac", "flac", "mp3", "m4a", "opus", "vorbis", "wav")
 $AutoSetupFormats = @("flac", "wav", "mp4", "webm")  # Don't include mp3, as this is the base script
-
-Write-Host $BinariesDir
